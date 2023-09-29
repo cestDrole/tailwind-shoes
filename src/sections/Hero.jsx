82613a3a014587dc26f4f84a73f9e4cbcc0e73d7
SJ <smwjasnosz@gmail.com>
2023-09-29 16:00:12 +0200

    Finish Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,14 +1,17 @@
 import Button from "../components/Button";
+import ShoeCard from "../components/ShoeCard";
 import { arrowRight } from "../assets/icons";
 import { shoes, statistics } from "../constants";
 import { bigShoe1 } from "../assets/images";
-import ShoeCard from "../components/ShoeCard";
+import { useState } from "react";
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
   return (
     <section
       id="home"
-      className="w-full flex xl:flex-row flex-col justify-center border-2 min-h-screen gap-10 max-container"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">
@@ -39,23 +42,23 @@ const Hero = () => {
       </div>
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
-          src={bigShoe1}
+          src={bigShoeImg}
           alt="shoe collection"
           width={610}
           height={500}
           className="object-contain relative z-10"
         />
-      </div>
-      <div>
-        {shoes.map((shoe) => (
-          <div key={shoe}>
-            <ShoeCard
-              imgUrl={shoe}
-              changeBigShoeImage={() => {}}
-              bigShoeImage=""
-            />
-          </div>
-        ))}
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] max-sm:px-6">
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard
+                imgUrl={shoe}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImage={bigShoeImg}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
