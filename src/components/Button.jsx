220eec74a8aa30dff945437368237894e2790714
SJ <smwjasnosz@gmail.com>
2024-01-20 17:37:38 +0100

    Button, hero rwd changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,24 +10,22 @@ const Button = ({
   return (
     <a
       href={link}
-      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none 
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none w-max transition duration-150 ease-in hover:bg-white hover:text-coral-red hover:border-coral-red
     ${
       backgroundColor
         ? `${backgroundColor} ${textColor} ${borderColor}`
         : "bg-coral-red text-white border-coral-red"
     } rounded-full ${fullWidth && "wfull"}`}
     >
-      <button className='flex justify-center items-center gap-2'>
-        {label}
+      {label}
 
-        {iconUrl && (
-          <img
-            src={iconUrl}
-            alt='arrow right icon'
-            className='ml-2 rounded-full w-5 h-5'
-          />
-        )}
-      </button>
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt='arrow right icon'
+          className='ml-2 rounded-full w-5 h-5'
+        />
+      )}
     </a>
   );
 };
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,45 +10,54 @@ const Hero = () => {
 
   return (
     <section
-      id="home"
-      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+      id='home'
+      className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p className="text-xl font-montserrat text-coral-red">
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-center sm:items-start w-full max-xl:padding-x pt-28 z-10'>
+        <p className='text-xl font-montserrat text-coral-red'>
           Our Summer Collection
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+        <h1 className='mt-10 font-palanquin text-4xl md:text-8xl max-sm:text-[72px] relative max-sm:leading-[82px] font-bold'>
+          <span className='xl:bg-white xl:whitespace-nowrap z-10 sm:pr-10 text-[35px] md:text-8xl'>
             The New Arrival
           </span>
           <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+          <span className='text-coral-red inline-block sm:mt-3 text-[35px] md:text-8xl'>
+            Nike
+          </span>{" "}
+          <span className='sm:mt-3 text-[35px] md:text-8xl'>Shoes</span>
         </h1>
-        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-10 max-w-sm text-left'>
           Discover stylish Nike arrivals, quality comfort and innovation for
           your active life.
         </p>
-        <Button label="Shop now" iconUrl={arrowRight} />
-        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+        <Button
+          label='Shop now'
+          iconUrl={arrowRight}
+          link='https://www.nike.com/pl/'
+        />
+        <div className='flex justify-center sm:justify-start items-start flex-wrap w-full mt-10 gap-8 sm:gap-16'>
           {statistics.map((stat) => (
             <div key={stat.label}>
-              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
-              <p className="leading-7 font-montserrat text-slate-gray">
+              <p className='text-2xl md:text-4xl font-palanquin font-bold'>
+                {stat.value}
+              </p>
+              <p className='leading-7 font-montserrat text-slate-gray'>
                 {stat.label}
               </p>
             </div>
           ))}
         </div>
       </div>
-      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
         <img
           src={bigShoeImg}
-          alt="shoe collection"
+          alt='shoe collection'
           width={610}
           height={500}
-          className="object-contain relative z-10 cursor-pointer"
+          className='object-contain relative z-10 cursor-pointer'
         />
-        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] max-sm:px-6">
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] max-sm:px-6'>
           {shoes.map((shoe) => (
             <div key={shoe}>
               <ShoeCard
