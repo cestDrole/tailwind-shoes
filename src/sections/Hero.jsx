86c61c3192f57d1298f7fafbad9a93d3652adcbc
SJ <smwjasnosz@gmail.com>
2024-01-21 15:57:38 +0100

    Hero rwd changes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -13,19 +13,19 @@ const Hero = () => {
       id='home'
       className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
     >
-      <div className='relative xl:w-2/5 flex flex-col justify-center items-center sm:items-start w-full max-xl:padding-x pt-28 z-10'>
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 z-10'>
         <p className='text-xl font-montserrat text-coral-red'>
           Our Summer Collection
         </p>
-        <h1 className='mt-10 font-palanquin text-4xl md:text-8xl max-sm:text-[72px] relative max-sm:leading-[82px] font-bold'>
-          <span className='xl:bg-white xl:whitespace-nowrap z-10 sm:pr-10 text-[35px] md:text-8xl'>
+        <h1 className='sm:mt-10 font-palanquin text-4xl md:text-8xl max-sm:text-[72px] relative font-bold'>
+          <span className='xl:bg-white xl:whitespace-nowrap z-10 sm:pr-10 text-[40px] md:text-8xl'>
             The New Arrival
           </span>
           <br />
-          <span className='text-coral-red inline-block sm:mt-3 text-[35px] md:text-8xl'>
+          <span className='text-coral-red inline-block sm:mt-3 text-[40px] md:text-8xl'>
             Nike
           </span>{" "}
-          <span className='sm:mt-3 text-[35px] md:text-8xl'>Shoes</span>
+          <span className='sm:mt-3 text-[40px] md:text-8xl'>Shoes</span>
         </h1>
         <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-10 max-w-sm text-left'>
           Discover stylish Nike arrivals, quality comfort and innovation for
@@ -36,7 +36,7 @@ const Hero = () => {
           iconUrl={arrowRight}
           link='https://www.nike.com/pl/'
         />
-        <div className='flex justify-center sm:justify-start items-start flex-wrap w-full mt-10 gap-8 sm:gap-16'>
+        <div className='flex justify-start items-start flex-wrap w-full mt-10 gap-8 sm:gap-16'>
           {statistics.map((stat) => (
             <div key={stat.label}>
               <p className='text-2xl md:text-4xl font-palanquin font-bold'>
